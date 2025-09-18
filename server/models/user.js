5042b9713735465e03a232926d21ac7c1efc92b0
gustavodaque <gustavo.ferraricneri@gmail.com>
2025-09-18 10:36:54 -0300

    primeiro commit

---
+++ server/models/user.js
@@ -0,0 +1,26 @@
+const bcrypt = require('bcryptjs');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    nome: DataTypes.STRING, // Nome do usuário
+    email: { 
+      type: DataTypes.STRING,
+      unique: true // Garante que o e-mail não se repita
+    },
+    senha: DataTypes.STRING, // Senha que será criptografada
+    cpf: { 
+      type: DataTypes.STRING,
+      unique: true // Garante que o CPF não se repita
+    },
+    telefone: DataTypes.STRING // Telefone do usuário
+  });
+
+// Hook que executa antes de criar um usuário
+// Usado para criptografar a senha antes de salvar no banc
+  // Antes de salvar o usuário, criptografa a senha
+  User.beforeCreate(async (user) => {
+    user.senha = await bcrypt.hash(user.senha, 10);  // Criptografa a senha
+  }); 
+
+  return User;
+};
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,10 +1,8 @@
-// routes/auth.js
-import express from 'express';
-import { register, login } from '../controllers/authController.js';
-
+const express = require('express');
 const router = express.Router();
+const authController = require('../controllers/authController');
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/registrar', authController.registrar);
+router.post('/login', authController.login);
 
-export default router;
+module.exports = router;
